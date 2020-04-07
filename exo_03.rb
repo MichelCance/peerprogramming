559f4fdb34c51b2b73a5954943a8a65352bc4bdc
MichelCance <mcance@leadsmaniac.com>
2020-04-07 10:48:50 +0200

    Ajout exos 3, 4 et 5

---
+++ exo_03.rb
@@ -0,0 +1,10 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne: Bonjour, monde !"
+
+# cela donne une ligne de commentaire si on a choisi de faire deux lignes différentes#
+
+puts "Bonjour, monde !
+
+#{}Et avec une voix sexy, ça donne: Bonjour, monde !"
+
+#Cela ouvre des accolades sit on a choisi de faire un seul bloc de texte #
