04a60f8b51a640043c92f4653e4a0da9e541679b
MichelCance <mcance@leadsmaniac.com>
2020-04-07 11:16:36 +0200

    Ajout commentaires

--- exo_05.rb
+++ exo_05.rb
@@ -1,22 +1,31 @@
 puts "On va compter le nombre d'heures de travail à THP"
+#Commentaire1= Affiche la ligne de texte demandée #
 puts "Travail : #{10 * 5 * 11}"
+#Commentaire2= concataine une opération dans le texte #
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#Commentaire3= idem que commentaire 2
 
 puts "Et en secondes ?"
-
+#Commentaire4: idem que commentiare1
 puts 10 * 5 * 11 * 60 * 60
+# Commentaire5: affiche le résultat de l'opération
 
 puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
-
+#Commentaire6: idem que commentaires 1 et 4
 puts 3 + 2 < 5 - 7
+#Commentaire7: retourne un booleen en comparant l'opération 3+2 avec 5-7
 
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#Commentaire8: Idem que commentaire2
 
 puts "Ok, c'est faux alors !"
+#Commentaire9: Idem que Commentaire1
 
 puts "C'est drôle ça, faisons-en plus :"
+#Commentaire10: Idem que Commentaire1
 
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+# Commentaire11: concataine des résultats de booleens dans le texte
